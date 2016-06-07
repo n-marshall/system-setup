e114e911d6aad92e26e0443edca8f9b42ba8adbd
Nicolas <nicolas@typeform.com>
2016-06-07 12:14:35 +0200

    osx added global gitignore

--- osx/osx-setup.sh
+++ osx/osx-setup.sh
@@ -34,11 +34,11 @@ clear
 
 echo "Installing Go..."
 brew install go --cross-compile-common
-tee -a ~/.bash_profile > /dev/null << EOL
-
+tee -a ~/.bash_profile > /dev/null <<EOL
 export GOPATH=\$HOME/dev/go
 export PATH=\$PATH:\$GOPATH/bin
 EOL
+
 source ~/.bash_profile
 go get -u golang.org/x/tools/cmd/godoc
 #go get -u golang.org/x/tools/cmd/vet
@@ -66,4 +66,48 @@ echo "Installing RVM..."
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 clear
+
+echo "Configuring Git..."
+tee ~/.gitignore_global > /dev/null <<EOL
+# Compiled source #
+###################
+*.com
+*.class
+*.dll
+*.exe
+*.o
+*.so
+
+# Packages #
+############
+# it's better to unpack these files and commit the raw source
+# git has its own built in compression methods
+*.7z
+*.dmg
+*.gz
+*.iso
+*.jar
+*.rar
+*.tar
+*.zip
+
+# Logs and databases #
+######################
+*.log
+*.sql
+*.sqlite
+
+# OS generated files #
+######################
+.DS_Store
+.DS_Store?
+._*
+.Spotlight-V100
+.Trashes
+ehthumbs.db
+Thumbs.db
+EOL
+git config --global core.excludesfile ~/.gitignore_global
+clear
+
 
