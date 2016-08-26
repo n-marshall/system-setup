02284269895b8a74e89a2a001a3aab294ceec07a
Nicolas <nicolas@typeform.com>
2016-08-26 10:48:03 +0200

    renamed scripts to setup.sh

---
+++ osx/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+read -p "Are you sure you want to run this installation script ? Press [Enter] for yes"
+
+echo "Installing XCode CLT..."
+xcode-select --install
+read -p "Press [Enter] key to once CLT finished..."
+clear
+
+echo "Creating dev environment..."
+mkdir ~/dev
+clear
+
+echo "Installing Homebrew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+clear
+
+source ./scripts/shell.sh
+source ./scripts/git.sh
+source ./scripts/go.sh
+source ./scripts/ruby.sh
+source ./scripts/text-editor.sh
+source ./scripts/docker-&-vms.sh
+source ./scripts/settings.sh
+
+echo "Installing basic stuff..." 
+brew install gpg python python3 wget graphviz
+clear
+
+echo "Installing Cask..."
+brew tap caskroom/cask
+clear
+
+echo "Installing Cask applications..."
+brew cask install alfred iterm2 ngrok cyberduck google-chrome vagrant visual-studio-code
+brew install hg bzr
+brew tap caskroom/versions
+brew cask install java7
+brew cask install java
+clear
+
+
+
+
+
+
+
