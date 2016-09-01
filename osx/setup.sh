35eb18b7dae8caf4e49189e8174ab5122b8c8ca6
Nicolas <nicolas@typeform.com>
2016-09-01 15:55:20 +0200

    improve osx setup

--- osx/setup.sh
+++ osx/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 read -p "Are you sure you want to run this installation script ? Press [Enter] for yes"
 
+source ./scripts/helper-functions.sh
+
 echo "Installing XCode CLT..."
 xcode-select --install
 read -p "Press [Enter] key to once CLT finished..."
 clear
 
-echo "Creating dev environment..."
-mkdir ~/dev
-clear
+source ./scripts/ruby.sh
 
 echo "Installing Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -18,7 +18,7 @@ clear
 source ./scripts/shell.sh
 source ./scripts/git.sh
 source ./scripts/go.sh
-source ./scripts/ruby.sh
+
 source ./scripts/text-editor.sh
 source ./scripts/docker-&-vms.sh
 source ./scripts/settings.sh
@@ -32,7 +32,7 @@ brew tap caskroom/cask
 clear
 
 echo "Installing Cask applications..."
-brew cask install alfred iterm2 ngrok cyberduck google-chrome vagrant visual-studio-code
+brew cask install alfred iterm2 ngrok cyberduck google-chrome vagrant visual-studio-code spotify
 brew install hg bzr
 brew tap caskroom/versions
 brew cask install java7
