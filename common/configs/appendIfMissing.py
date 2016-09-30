e837ed19017ee7f8375df16a4b9157679e0e3ba7
Nicolas <nicolas@typeform.com>
2016-09-30 14:34:32 +0200

    update appendIfMissing to handle URLs/Files/Strings

--- common/configs/appendIfMissing.py
+++ common/configs/appendIfMissing.py
@@ -1,33 +1,68 @@
-import re, os, subprocess, mmap, sys, pprint
+import re, os, subprocess, mmap, sys, urllib2, pprint
 
-def isFile1InFile2(file1Path, file2Path):
-    with open(file2Path) as file2:
-        file2Access = mmap.mmap(file2.fileno(), 0, access=mmap.ACCESS_READ)
-        file1Contents = open(file1Path).read()
-        if file2Access.find(file1Contents) != -1:
-            return True
-        else:
-            return False
+URL_REGEX = r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
+SOURCE_TYPE_URL = 'URL'
+SOURCE_TYPE_FILE = 'file'
+SOURCE_TYPE_STRING = 'string'
+
+def isSourceContentsInDestFile(sourcePath, destFileFullPath, sourceType):
+    if sourceType == SOURCE_TYPE_URL:
+        sourceContents = urllib2.urlopen(sourcePath).read()
+    elif sourceType == SOURCE_TYPE_FILE:
+        sourceContents = open(sourcePath).read()
+    elif sourceType == SOURCE_TYPE_STRING:
+        sourceContents = sourcePath
+
+    print destFileFullPath
+    if os.path.isfile(destFileFullPath) and os.stat(destFileFullPath).st_size != 0:
+        print os.path.isfile(destFileFullPath)
+        print os.stat(destFileFullPath).st_size
+        with open(destFileFullPath) as destFile:
+            print destFile.read()
+            destFileAccess = mmap.mmap(destFile.fileno(), 0, access=mmap.ACCESS_READ)
+            if destFileAccess.find(sourceContents) != -1:
+                return True
+            else:
+                return False
+    else:
+        return False
+        print 'file is empty'
+
+
+def append(source, destFileFullPath, sourceType):
+    if sourceType == SOURCE_TYPE_URL:
+        appendFunction = 'appendFromURL'
+    elif sourceType == SOURCE_TYPE_FILE:
+        appendFunction = 'appendFromFile'
+    elif sourceType == SOURCE_TYPE_STRING:
+        appendFunction = 'appendFromString'
+
+    command = ' '.join(['source', './common/configs/.shell-functions', '&&', appendFunction, source, destFileFullPath])
+    os.system(command)
+
 
 def appendIfMissing(source, dest):
-    destFullPath = os.path.expanduser(dest)
-    if os.path.isfile(destFullPath):
-        if isFile1InFile2(source, destFullPath):
+    if re.match(URL_REGEX, source, flags=0):
+        sourceType = SOURCE_TYPE_URL
+    elif os.path.isfile(source):
+        sourceType = SOURCE_TYPE_FILE
+    else:
+        sourceType = SOURCE_TYPE_STRING
+
+    destFileFullPath = os.path.expanduser(dest)
+    if os.path.isfile(destFileFullPath):
+        if isSourceContentsInDestFile(source, destFileFullPath, sourceType):
             print ('Source\'s contents found in dest file, no need to append')
         else:
             print('Source\'s contents cannot be found in dest file, appending...')
-            # append source file to destfile
-            command = ' '.join(['source', './common/configs/.shell-functions', '&&', 'catAndAppend', source, destFullPath])
-            os.system(command)
+            append(source, destFileFullPath, sourceType)
 
     else:
         print "destfile not a file yet, copying sourcefile to destfile..."
-        # copy source file to destfile
-        command = ' '.join(['source', './common/configs/.shell-functions', '&&', 'catAndAppend', source, destFullPath])
-        print command
-        os.system(command)
+        append(source, destFileFullPath, sourceType)
+
 
 if len(sys.argv) != 3:
-        sys.exit('[ERROR] number of arguments passed is not 3')
+    sys.exit('[ERROR] appendIfMissing.py, line 31: number of arguments passed is not 3')
 else:
     appendIfMissing(sys.argv[1], sys.argv[2])
