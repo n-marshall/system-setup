542b9689fe000db58d955e80605bcf999d4f6499
Nicolas <nicolas@typeform.com>
2016-10-17 17:25:17 +0200

    osx: update docker install to use installDmgFromUrl

--- setups/osx/docker-&-vms.sh
+++ setups/osx/docker-&-vms.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+[[ -z $repo ]] && repo=$(cd $(dirname $BASH_SOURCE[0]) && cd ../.. && pwd)
+
+. "${repo}"/configs/shell-functions.sh
+. "${repo}"/configs/osx/shell-functions.sh
+
 echo "Installing Docker and Vagrant..."
 brew cask install virtualbox
 brew cask install vagrant
-brew cask install vagrant-manager
-brew install docker
-brew install docker-compose
+
+installDmgFromUrl 'https://download.docker.com/mac/stable/Docker.dmg'
 
 appendIfMissing ../configs/.shrc.docker ~/.shrc
 clear
