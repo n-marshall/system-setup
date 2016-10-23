bcf0292dc60a83b595b21e4a51bb676bceedb38c
Nicolas Marshall <marshall.nicolas@gmail.com>
2016-10-24 01:52:53 +0200

    ubuntu: fix docker install

--- setups/ubuntu/virtualization.sh
+++ setups/ubuntu/virtualization.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
+[[ -z $repo ]] && repo=$(cd $(dirname $BASH_SOURCE[0]) && cd ../.. && pwd)
+
+. "${repo}"/configs/ubuntu/shell-functions.sh
+
 # virtualbox
 sudo apt-get install -y virtualbox
 sudo apt-get install -y dkms
 
 # docker
-curl -fsSL https://get.docker.com/ | sh
-sudo usermod -aG docker ubuntu
-## docker-compose
-sudo -i
-curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-exit
+# curl -fsSL https://get.docker.com/ | sh
+# sudo usermod -aG docker ubuntu
+sudo apt install -y docker.io
 ## vagrant
 wgetAndDpkg "https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb"
 ## otto / azk
