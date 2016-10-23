62978b7bd2d952a8d4504d85817073ca0bc375bf
Nicolas Marshall <marshall.nicolas@gmail.com>
2016-10-24 01:50:06 +0200

    fix rambox install

--- setups/ubuntu/rambox.sh
+++ setups/ubuntu/rambox.sh
@@ -4,12 +4,18 @@
 . "${repo}"/configs/shell-functions.sh
 . "${repo}"/configs/ubuntu/shell-functions.sh
 
+sudo apt-get install libappindicator1
+
 LATEST="0.4.4"
 URL="https://github.com/saenzramiro/rambox/releases/download/${LATEST}/Rambox-${LATEST}-x64.zip"
-echo ${URL}
 
-getAndUnzip ${URL} ~/.local/share/rambox
+INSTALL_DIR=~/.local/share/rambox
+getAndExtract ${URL} ${INSTALL_DIR}
 
-appendIfMissing "${repo}"/ubuntu/configs/rambox.desktop /usr/share/applications/rambox.desktop
+chmod +x ${INSTALL_DIR}/linux-unpacked/Rambox
 
+cp -r "${repo}"/customization/sparrow-icons ${INSTALL_DIR}/icons
 
+appendIfMissing "${repo}"/configs/ubuntu/rambox.desktop ~/.local/share/applications/rambox.desktop
+chmod +x ~/.local/share/applications/rambox.desktop
+sudo update-desktop-database
