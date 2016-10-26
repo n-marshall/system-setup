c541f25e9369f4f1e19fb1cb7777ee2ae63849d7
Nicolas Marshall <marshall.nicolas@gmail.com>
2016-10-27 01:12:41 +0200

    update golang setup

--- setups/ubuntu/programming-languages.sh
+++ setups/ubuntu/programming-languages.sh
@@ -10,13 +10,13 @@ sudo apt-get install -y python-pip
 sudo apt-get install -y ruby-full
 
 # golang
-GOLANG_LATEST='1.7.1'
+GOLANG_LATEST='1.7.3'
 sudo rm -rf /usr/local/go #remove any older version of go
-cd ~
-wget "https://storage.googleapis.com/golang/go${GOLANG_LATEST}.linux-amd64.tar.gz" #replace with any newer version of go
-sudo tar -C /usr/local -xzf "go${GOLANG_LATEST}.linux-amd64.tar.gz"
-rm "go${GOLANG_LATEST}.linux-amd64.tar.gz"
-appendIfMissing "${repo}"/configs/.shrc.gopath ~/.shrc
+TMPFILE=`mktemp`
+wget "https://storage.googleapis.com/golang/go${GOLANG_LATEST}.linux-amd64.tar.gz" -O ${TMPFILE} 
+sudo tar -C /usr/local -xzf ${TMPFILE}
+rm ${TMPFILE}
+appendIfMissing "${repo}"/configs/.shrc.gopath ~/.shrc && . ~/shrc
 
 # java
 sudo add-apt-repository -y ppa:webupd8team/java
@@ -27,4 +27,4 @@ sudo apt-get install -y oracle-java7-installer oracle-java7-set-default
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
-## webpack
+## webpack /broccoli /brunch
