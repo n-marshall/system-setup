5035b19dc819a9885202f529a43fff79297704e8
Nicolas Marshall <marshall.nicolas@gmail.com>
2017-03-24 14:34:54 +0100

    updated dir structure

--- setups/ubuntu/dir-structure.sh
+++ setups/ubuntu/dir-structure.sh
@@ -1,4 +1,4 @@
-declare -a arr=("dev" "Documents" "Movies & Shows" "Graphism" "Books" "Windows/installers" "Windows/portable" "Music" "Photos" "photos_autres" "downloads")
+declare -a arr=("dev" "Documents" "Movies & Shows" "Graphism" "Books" "Windows" "Music" "Photos" "photos_autres" "downloads")
 for i in "${arr[@]}"
 do
     mkdir -p ~/"$i"
